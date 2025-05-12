78964a84f4ec4859dcd773422c011e2166ac0a57
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-05-12 21:54:04 +0100

    Project created, Dependencies resolved

---
+++ HireCraft/src/test/java/com/example/HireCraft/HireCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HireCraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
